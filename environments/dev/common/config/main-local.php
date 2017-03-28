72f5a4a3ab7457faa6aae8b0c9b9c7b41f92dad6
Johan Kladder <j.kladder@st.hanze.nl>
2017-03-28 09:30:40 +0200

    + config support main-local

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -1,20 +1,27 @@
 <?php
+
+use Symfony\Component\Yaml\Yaml;
+
+/*
+  Obtain the secrets file to ensure safety. Please fill in the database entry
+  when creating an environment localy.
+*/
+
+$yaml = Yaml::parse(file_get_contents(__DIR__ . "/config.yml"));
+$database = $yaml['database'];
+
 return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => "mysql:host={$database['location']};dbname=" . $database['name'],
+            'username' => $database['username'],
+            'password' => $database['password'],
             'charset' => 'utf8',
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
         ],
     ],
 ];
--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -1,11 +1,22 @@
 <?php
+
+use Symfony\Component\Yaml\Yaml;
+
+/*
+  Obtain the secrets file to ensure safety. Please fill in the database entry
+  when creating an environment localy.
+*/
+
+$yaml = Yaml::parse(file_get_contents(__DIR__ . "/config.yml"));
+$database = $yaml['database'];
+
 return [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
+            'dsn' => "mysql:host={$database['location']};dbname=" . $database['name'],
+            'username' => $database['username'],
+            'password' => $database['password'],
             'charset' => 'utf8',
         ],
         'mailer' => [
