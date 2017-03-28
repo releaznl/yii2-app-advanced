776b0c99772b4efc37f03f687ede376aecd19e24
Johan Kladder <j.kladder@st.hanze.nl>
2017-03-28 11:13:42 +0200

    + Added basic host iterations

--- deploy.php
+++ deploy.php
@@ -3,24 +3,35 @@ namespace Deployer;
 
 require 'recipe/yii.php';
 
+use Symfony\Component\Yaml\Yaml;
+
 // Configuration
 
-set('repository', 'git@domain.com:username/repository.git');
+$yaml = Yaml::parse(file_get_contents(__DIR__ . "/deploy-config.yml"));
+
+$general = $yaml['general'];
+set('repository', $general['ssh_repo_url']);
 set('git_tty', true); // [Optional] Allocate tty for git on first deployment
-add('shared_files', []);
-add('shared_dirs', []);
-add('writable_dirs', []);
+set('shared_files', []);
+set('shared_dirs', []);
+set('writable_dirs', []);
 
 // Hosts
+foreach($yaml['server'] as $host) {
+  host($host['host'])
+      ->stage($host['stage'])
+      ->set('deploy_path', '/var/www/project.com');
+}
 
-host('project.com')
-    ->stage('production')
-    ->set('deploy_path', '/var/www/project.com');
+// Tasks
 
-host('dev.project.com')
-    ->stage('develop')
-    ->set('deploy_path', '/var/www/project.com');
-
+desc('Restart PHP-FPM service');
+task('php-fpm:restart', function () {
+    // The user must have rights for restart service
+    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
+    run('sudo systemctl restart php-fpm.service');
+});
+after('deploy:symlink', 'php-fpm:restart');
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
