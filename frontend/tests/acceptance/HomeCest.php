b224aaa81ab4a4152a4a3f1d98a5d18c4334606f
Johan Kladder <j.kladder@st.hanze.nl>
2017-03-28 08:58:50 +0200

    - Removed unused tests

--- frontend/tests/acceptance/HomeCest.php
+++ frontend/tests/acceptance/HomeCest.php
@@ -11,11 +11,5 @@ class HomeCest
         $I->wait(1);
         $I->amOnPage(Url::to('/site/index'));
         $I->see('My Company');
-
-        // $I->seeLink('About');
-        // $I->click('About');
-        // $I->wait(2); // wait for page to be opened
-        //
-        // $I->see('This is the About page.');
     }
 }
