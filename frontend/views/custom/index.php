f2b619f6c1afafb09d2a39d539436b5b65bb474a
Johan Kladder <j.kladder@st.hanze.nl>
2017-03-29 11:41:13 +0200

    + Added custom guide entry + tests

---
+++ frontend/views/custom/index.php
@@ -0,0 +1,28 @@
+<div class="jumbotron">
+    <h1>Welcome to the custom guide!</h1>
+</div>
+
+<div class="body-content">
+
+    <div class="row">
+        <div class="col-lg-4">
+            <h2>Heading</h2>
+
+            <p></p>
+
+        </div>
+        <div class="col-lg-4">
+            <h2>Heading</h2>
+
+            <p></p>
+
+        </div>
+        <div class="col-lg-4">
+            <h2>Heading</h2>
+
+            <p></p>
+
+        </div>
+    </div>
+
+</div>
